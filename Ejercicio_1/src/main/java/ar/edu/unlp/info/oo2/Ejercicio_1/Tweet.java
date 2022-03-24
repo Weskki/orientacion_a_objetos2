0c19968c9d9614e9625735b8a9a77e993d702054
Julian Gomez <gomezjulian1995@gmail.com>
2022-03-24 17:38:14 -0300

    Ejercicio1 completo(faltan los test)

--- Ejercicio_1/src/main/java/ar/edu/unlp/info/oo2/Ejercicio_1/Tweet.java
+++ Ejercicio_1/src/main/java/ar/edu/unlp/info/oo2/Ejercicio_1/Tweet.java
@@ -8,4 +8,28 @@ public class Tweet implements Retweeteable {
 		this.texto = text;
 		this.autor = user;
 	}
+
+	@Override
+	public String getText() {
+		return null;
+	}
+
+	@Override
+	public Usuario getUsuario() {
+		return null;
+	}
+
+	@Override
+	public Retweet retweet() {
+		return new Retweet(this);
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public Usuario getAutor() {
+		return autor;
+	}
+
 }
--- Ejercicio_1/src/main/java/ar/edu/unlp/info/oo2/Ejercicio_1/Usuario.java
+++ Ejercicio_1/src/main/java/ar/edu/unlp/info/oo2/Ejercicio_1/Usuario.java
@@ -2,10 +2,11 @@ package ar.edu.unlp.info.oo2.Ejercicio_1;
 import java.util.*;
 
 public class Usuario {
-	
+
 	private String screenName;
 	private String eMail;
 	private Set<Tweet> tweets;
+	private Set<Retweet> retweets;
 	
 	public Usuario(String unNombre) {
 		this.screenName = unNombre;
@@ -16,4 +17,34 @@ public class Usuario {
 		return tweets.add(nuevoTweet);
 	}
 	
+	public Retweet retweetear(Retweeteable unRetweeteable) {
+		Retweet nuevoRetweet = unRetweeteable.retweet();
+		retweets.add(nuevoRetweet);
+		return nuevoRetweet;
+	}
+	
+	public boolean eliminarTweet(Tweet unTweet) {
+		return tweets.remove(unTweet);
+	}
+	
+	public String getScreenName() {
+		return screenName;
+	}
+
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public Set<Tweet> getTweets() {
+		return tweets;
+	}
+
 }
